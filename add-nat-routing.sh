6c45400f0957933f08ea613f4a11b50c0f5956dc
Moon Kim <76114025+moon-kim@users.noreply.github.com>
2022-11-25 23:51:44 +0100

    Update add-nat-routing.sh

--- add-nat-routing.sh
+++ add-nat-routing.sh
@@ -8,15 +8,15 @@ SUB_NET="10.10.1.0/24"          # WG IPv4 sub/net aka CIDR
 WG_PORT="51820"                  # WG udp port
 SUB_NET_6="fd42:42:42::/64"      # WG IPv6 sub/net
 
-# IPv4 rules #
-$IPT -t nat -D POSTROUTING -s $SUB_NET -o $IN_FACE -j MASQUERADE
-$IPT -D INPUT -i $WG_FACE -j ACCEPT
-$IPT -D FORWARD -i $IN_FACE -o $WG_FACE -j ACCEPT
-$IPT -D FORWARD -i $WG_FACE -o $IN_FACE -j ACCEPT
-$IPT -D INPUT -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
+## IPv4 ##
+$IPT -t nat -I POSTROUTING 1 -s $SUB_NET -o $IN_FACE -j MASQUERADE
+$IPT -I INPUT 1 -i $WG_FACE -j ACCEPT
+$IPT -I FORWARD 1 -i $IN_FACE -o $WG_FACE -j ACCEPT
+$IPT -I FORWARD 1 -i $WG_FACE -o $IN_FACE -j ACCEPT
+$IPT -I INPUT 1 -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
 
-# IPv6 rules (uncomment) #
-$IPT6 -t nat -D POSTROUTING -s $SUB_NET_6 -o $IN_FACE -j MASQUERADE
-$IPT6 -D INPUT -i $WG_FACE -j ACCEPT
-$IPT6 -D FORWARD -i $IN_FACE -o $WG_FACE -j ACCEPT
-$IPT6 -D FORWARD -i $WG_FACE -o $IN_FACE -j ACCEPT
+## IPv6 (Uncomment) ##
+$IPT6 -t nat -I POSTROUTING 1 -s $SUB_NET_6 -o $IN_FACE -j MASQUERADE
+$IPT6 -I INPUT 1 -i $WG_FACE -j ACCEPT
+$IPT6 -I FORWARD 1 -i $IN_FACE -o $WG_FACE -j ACCEPT
+$IPT6 -I FORWARD 1 -i $WG_FACE -o $IN_FACE -j ACCEPT
