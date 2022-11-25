7824fc24fbac26726536bc2188d3bb9371bc488e
Moon Kim <76114025+moon-kim@users.noreply.github.com>
2022-11-26 00:27:36 +0100

    Update add-nat-routing.sh

--- add-nat-routing.sh
+++ add-nat-routing.sh
@@ -2,7 +2,7 @@
 IPT="/sbin/iptables"
 IPT6="/sbin/ip6tables"
 
-IN_FACE="enp0s3"                   # NIC connected to the internet
+IN_FACE="enp0s3"                   # NIC connected to the internet. command #ip -o -4 route show to default | awk '{print $5}'
 WG_FACE="wg0"                    # WG NIC
 SUB_NET="10.10.1.0/24"          # WG IPv4 sub/net aka CIDR
 WG_PORT="51820"                  # WG udp port
